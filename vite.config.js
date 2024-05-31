8b804b37d0734b1361f5131893b1e8c4597dba53
Franco Buceta <francobuceta95@gmail.com>
2024-05-31 19:21:48 -0300

    add server config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Permite que el servidor sea accesible desde fuera del contenedor docker.
+    port: 8080,
+    strictPort: true,
+    origin: "http://localhost:8080", // Esta es la URL de origen externa.
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+    host: "0.0.0.0", // También permite que el servidor de previsualización sea accesible desde fuera del contenedor.
+  },
+});
